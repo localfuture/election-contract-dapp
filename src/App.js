027c9b47da8d3871d257a494d63d07c9dc7909d5
localfuture <anandalex1993@gmail.com>
2021-03-02 18:06:12 +0530

    frontend basic

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Election Contract</h1>
+        <p>Ethereum Dapp</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
